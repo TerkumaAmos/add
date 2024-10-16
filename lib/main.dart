c38b7f98f494eb9f779dfd72cf59fdadf1d11620
TerkumaAmos <agberterkuma2020@gmail.com>
2024-10-16 15:30:38 -0700

    rebranch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 void main(){
   return runApp(
     MaterialApp(
@@ -17,25 +16,6 @@ void main(){
   );
 }
 
-=======
-
-void main() {
-  return runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.teal.shade200,
-          appBar: AppBar(
-            title: Text("Dices"),
-            backgroundColor: Colors.blue,
-          ),
-          body: DicePage(),
-        ),
-      )
-  );
-}
-
-
->>>>>>> aa8f3c68ec9e9099c96efc46282676491966973a
 class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
@@ -43,7 +23,6 @@ class DicePage extends StatefulWidget {
   State<DicePage> createState() => _DicePageState();
 }
 
-<<<<<<< HEAD
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber =4;
@@ -83,46 +62,10 @@ class _DicePageState extends State<DicePage> {
                   changeDiceFace();
                 },
                 child: Image.asset("images/dice$leftDiceNumber.jpeg",
-                  width: 200.0,
+                  width: 100.0,
                 ),
               ),
             ),
-=======
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber =1;
-  int rightDiceNumber =3;
-
-  void changeDiceFace(){
-
-    setState(() {
-      leftDiceNumber=Random().nextInt(6)+1;
-      rightDiceNumber=Random().nextInt(6)+1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Center(
-      child: Row(
-        children:<Widget> [
-          Expanded(
-            //flex:2,
-            child: TextButton(
-              onPressed: () {
-                changeDiceFace();
-              },
-              child: Image.asset("images/dice$leftDiceNumber.jpeg"),),
-          ),
-          Expanded(
-            //flex: 1,
-            child: TextButton(
-                onPressed: () {
-              changeDiceFace();
-
-                },
-                child: Image.asset("images/dice$rightDiceNumber.jpeg")),
->>>>>>> aa8f3c68ec9e9099c96efc46282676491966973a
           ),
         ],
       ),
