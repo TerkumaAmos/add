6f6b0b9fb8fa7686dd8a7156fc4225aae5c74d0a
TerkumaAmos <agberterkuma2020@gmail.com>
2024-10-16 13:50:02 -0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main(){
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Amos Biography'),
-            backgroundColor: const Color.fromARGB(255, 65, 16, 73),
-          ),
-          body: Container(
-              margin: const EdgeInsets.all(8.0),
-              child: Padding(
-                padding: const EdgeInsets.all(9.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          height: 200,
-                          width: 700,
-                          color: Colors.white,
-                          child: const Text('ABOUT',
-                              style: TextStyle(color: Colors.black)),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 200,
-                          width: 700,
-                          color: Colors.blue,
-                          child: const Text(
-                            'ABOUT',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 200,
-                          width: 700,
-                          color: Colors.blue,
-                          child: const Text('ABOUT',
-                              style: TextStyle(color: Colors.black)),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              )),
-        ),
       ),
-    );
-  }
+    )
+  );
 }
